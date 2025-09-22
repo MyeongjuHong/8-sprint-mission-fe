a429336777d0e52ba1b6d4d3cba7b782fecc8d07
MyeongjuHong <hmjpearl@gmail.com>
2025-09-22 11:03:25 +0900

    feat: Add 상품 등록 페이지

---
+++ src/components/ItemCard.jsx
@@ -0,0 +1,9 @@
+function ItemCard({ imageURL = "../assets/images/img_default.svg" }) {
+  return (
+    <>
+      <img src={imageURL} />
+    </>
+  );
+}
+
+export default ItemCard;
--- src/pages/Items/index.jsx
+++ src/pages/Items/index.jsx
@@ -1,7 +1,22 @@
+import SmallButton from "@/components/Button/SmallButton";
+import { Link } from "react-router-dom";
+
 function Items() {
   return (
     <>
-      <div></div>
+      <div>
+        <div>판매 중인 상품</div>
+        <div>
+          <input type="text" />
+          <Link to="/registration">
+            <SmallButton>상품 등록하기</SmallButton>
+          </Link>
+          <select>
+            <option default>최신순</option>
+            <option>좋아요순</option>
+          </select>
+        </div>
+      </div>
       <div></div>
       <div></div>
     </>
